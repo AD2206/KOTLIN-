fe5e39f61622e6ceecc60f87cf63b0d021cb3bb1
Aryan Dubey <66486350+AD2206@users.noreply.github.com>
2022-03-19 21:10:46 +0530

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    println("Hello World")
+}
+//In kotlin statements do not have to end with a semicolon (;)
+//Any code inside the main() { } will be executed
+//file should be saved with .kt
