3c00fa814babe29800d70636bf365458453c22ba
Aryan Dubey <66486350+AD2206@users.noreply.github.com>
2022-03-19 21:11:06 +0530

    Add files via upload

---
+++ Variable_1.kt
@@ -0,0 +1,14 @@
+  //var //val
+  //in var (keyword can be modified)
+  //in val(keyword cannot be modified)
+  //Following code below is a  basic example code. An introduction to variables in kotlin
+  fun main() {
+      var name = "Aryan" //string (text)
+      val birthyear = 2006 //int (number)    
+
+      println(name)
+      println(birthyear)
+  }
+  //Kotlin understands Aryan is a string and 2006 is an integer
+  //However it is optional to specify
+  //An variable can be declared without assigning a value. However, for this case the type must be specified otherwise it will generate an error
