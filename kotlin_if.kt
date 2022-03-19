9f1776d4ca22608db9873b040578a1be8bc9d5a7
Aryan Dubey <66486350+AD2206@users.noreply.github.com>
2022-03-19 21:11:42 +0530

    Add files via upload

---
+++ kotlin_if.kt
@@ -0,0 +1,15 @@
+fun main() {
+    if (20>18) {
+        println("20 is greatr than 18")
+
+    }
+
+}
+
+//code with a variable
+//fun main() {
+//val x = 20
+//val y = 18
+//if (x > y) {
+   // println("x is greater than y")
+//} 
